b02038228800bb2f4b4dc7d7f12be301432d0df0
Åmund Henriksen <aamundhe@getacademy.no>
2022-11-11 13:46:29 +0100

    lagt til kommentarer og debugga

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
-// Variabler
+// Variabler til HTML ID
 let faceExpression = document.getElementById("face-expression")
 let playText = document.getElementById("play-text")
 let feedText = document.getElementById("feed-text")
 let sleepText = document.getElementById('sleep-text')
+
+// Variabler til status
 let playStatus
 let feedStatus
 let sleepStatus
-let totalStatus
 let averageMood
+let isAlive = false
 
-// Intervaller
+// Variabler til intervaller
 let playInterval
 let feedInterval
 let sleepInterval
 
+// Funksjon som returnerer tifeldig tall mellom to parameter
 function randomNumber(min, max) {
     return min + Math.floor(Math.random() * (max - min + 1))
 }
 
+// Funksjon som øker variabelen med 1 dersom variabelen er mindre enn 100
 function play() {
-    if (playStatus < 100) {
+    if (playStatus < 100 && isAlive === true) {
         playStatus++
         playText.innerHTML = `${playStatus}%`
     }
 }
 
 function feed() {
-    if (feedStatus < 100) {
+    if (feedStatus < 100 && isAlive === true) {
         feedStatus++
         feedText.innerHTML = `${feedStatus}%`
     }
 }
 
 function sleep() {
-    if (sleepStatus < 100) {
+    if (sleepStatus < 100 && isAlive === true) {
         sleepStatus++
         sleepText.innerHTML = `${sleepStatus}%`
     }
 }
 
-function reset() {
-    playStatus = randomNumber(50, 80)
-    sleepStatus = randomNumber(50, 80)
-    feedStatus = randomNumber(50, 80)
-
-    playInterval = setInterval(decreasePlay, 20)
-    feedInterval  = setInterval(decreaseFeed, 20)
-    sleepInterval = setInterval(decreaseSleep, 20)
+// Funksjon som setter status verdiene og starter intervallene
+function startOrReset() {
+    if (isAlive === false) {
+        playStatus = randomNumber(50, 80)
+        sleepStatus = randomNumber(50, 80)
+        feedStatus = randomNumber(50, 80)
+    
+        playInterval = setInterval(decreasePlay, 1000)
+        feedInterval  = setInterval(decreaseFeed, 1000)
+        sleepInterval = setInterval(decreaseSleep, 1000)
+        isAlive = true
+    }
 }
 
+// Funksjon som reduserer variabelen med 1 og stopper om variabelen når 0
 function decreasePlay() {
     playStatus--
 
@@ -82,12 +91,14 @@ function decreaseSleep() {
     setAverageMood() 
 }
 
+// Funksjon som regner ut gjennomsnittet av de tre statusene og lagrer det i en variabel
 function setAverageMood() {
     averageMood = (playStatus + feedStatus + sleepStatus) / 3
     console.log(`Average percent: ${averageMood}`)
     setFacialExpression()
 }
 
+// Funksjon som setter ansiktsutrykk ut ifra averageMood sin verdi
 function setFacialExpression() {
     if (averageMood < 100) {
         faceExpression.innerHTML = "😀"
@@ -103,16 +114,6 @@ function setFacialExpression() {
         clearInterval(playInterval)
         clearInterval(feedInterval)
         clearInterval(sleepInterval)
-        play = null
-        feed = null
-        sleep = null
+        isAlive = false
     }
 }
-
-
-
-
-
-
-
-
